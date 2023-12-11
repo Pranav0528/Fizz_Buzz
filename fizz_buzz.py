e978788a7938c24b5309158325f83e56c4626fb4
Pranav Kulkarni <78878540+Pranav0528@users.noreply.github.com>
2023-12-11 22:29:03 +0530

    Add files via upload

---
+++ fizz_buzz.py
@@ -0,0 +1,10 @@
+number = 100
+for i in range(1, number+1):
+    if (i % 5 == 0) and (i % 3 == 0):
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
